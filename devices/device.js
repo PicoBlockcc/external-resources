a87881d38367c791ad9fa701333576b5ca35049b
PicoBlock <mhamed.jamil@outlook.com>
2022-04-29 11:39:23 +0000

    first commit

---
+++ devices/device.js
@@ -0,0 +1,13 @@
+module.exports = [
+    // Buid-in device
+    'arduinoUno',
+    'arduinoNano_arduinoUno',
+    'arduinoMini_arduinoUno',
+    'microbit',
+    'microbitV2',
+    'makeyMakey',
+    // Third party
+    'ironKit_arduinoUno',
+    'QDPRobot_arduinoUnoUltra',
+    'QDPRobotC02_arduinoEsp32'
+];
